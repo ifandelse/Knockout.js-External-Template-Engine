08f767a99547536b61221f362071df8ce23e551a
Josh Bush <josh@digitalbush.com>
2011-03-25 20:49:56 -0500

    Pointing at uglifyjs service.

--- build/build-windows.bat
+++ build/build-windows.bat
@@ -16,19 +16,15 @@ goto :EOF
 :Combine
 type %AllFiles%                   > %OutDebugFile%.temp
 
-@rem Now call Google Closure Compiler to produce a minified version
+@rem Now call UglifyJS to produce a minified version
 copy /y version-header.js %OutMinFile%
-tools\curl -d output_info=compiled_code -d output_format=text -d compilation_level=ADVANCED_OPTIMIZATIONS --data-urlencode js_code@%OutDebugFile%.temp "http://closure-compiler.appspot.com/compile" > %OutMinFile%.temp
+tools\curl --data-urlencode js_code@%OutDebugFile%.temp "http://marijnhaverbeke.nl/uglifyjs" > %OutMinFile%.temp
 
 @rem Finalise each file by prefixing with version header and surrounding in function closure
 copy /y version-header.js %OutDebugFile%
-echo (function(window,undefined){ >> %OutDebugFile%
 type %OutDebugFile%.temp		  >> %OutDebugFile%
-echo })(window);                  >> %OutDebugFile%
 del %OutDebugFile%.temp
 
 copy /y version-header.js %OutMinFile%
-echo (function(window,undefined){ >> %OutMinFile%
 type %OutMinFile%.temp		  	  >> %OutMinFile%
-echo })(window);                  >> %OutMinFile%
 del %OutMinFile%.temp
