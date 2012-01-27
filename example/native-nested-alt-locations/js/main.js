c1ff5a3c512182f3f70250f0936dcc0135076446
Jim Cowart <jim@ifandelse.com>
2012-01-26 20:23:43 -0500

    Added sample project demonstrating nested templates using binding options overriding template url location, etc.

---
+++ example/native-nested-alt-locations/js/main.js
@@ -0,0 +1,39 @@
+var MockedConfigObject = {
+    templateLocations: {
+        main: "templates",
+        alt: "/example/native-nested-alt-locations/templates-alt/city"
+    }
+};
+
+$(function() {
+    infuser.defaults.templateUrl = MockedConfigObject.templateLocations.main;
+    infuser.defaults.templatePrefix = "";
+    infuser.defaults.templateSuffix = ".html";
+
+    function State(id, name, cities) {
+        return {
+            id: ko.observable(id),
+            name: ko.observable(name),
+            cities: ko.observableArray(cities || ["Dunno"])
+        }
+    }
+    var viewModel = {
+        isEditable: ko.observable(false),
+        items: ko.observableArray([
+                new State(1, "Tennessee", [
+                    {name: "Nashville"}, {name: "Chattanooga"}, {name: "Franklin"}, {name: "Spring Hill"}, {name: "Signal Mountain"}
+                ]),
+                new State(2, "Georgia", [
+                    {name: "Atlanta"}, {name: "Savannah"}, {name: "Snellville"}, {name: "Stone Mountain"}
+                ]),
+                new State(3, "Texas", [
+                    {name: "Dallas"}, {name: "Austin"}, {name: "Houston"}, {name: "San Antonio"}, {name: "Katy"}
+                ])
+            ]),
+        whichTemplateToUse: function() {
+            return viewModel.isEditable() ? 'edit' : 'view';
+        }
+    };
+
+    ko.applyBindings(viewModel);
+});
