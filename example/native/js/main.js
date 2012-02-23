09ab14e814270f5aefe9a17250d01c4f9c61e637
Jim Cowart <ashbylane@gmail.com>
2012-02-23 18:30:59 -0500

    Added an afterRender example to the native example page

--- example/native/js/main.js
+++ example/native/js/main.js
@@ -7,7 +7,18 @@ $(function() {
         return {
             id: ko.observable(id),
             name: ko.observable(name),
-            cities: ko.observableArray(cities || ["Dunno"])
+            cities: ko.observableArray(cities || ["Dunno"]),
+	        afterRenderCallback: function(elem, model) {
+		        if($(elem).is(".infuser-loading")) {
+			        console.log("afterAdd invoked but this is still the loading template");
+		        }
+		        else {
+		            console.log("afterAdd invoked for " + model.name + ", " + name + ".  This is the real template.");
+			        if($(elem).find("strong").text() === "Chattanooga") {
+				        $(elem).find("strong").html("<a href='http://www.chattanoogafun.com/' alt='O Hai, Chattanooga' target='_blank'>Chattanooga</a>");
+			        }
+		        }
+	        }
         }
     }
     var viewModel = {
